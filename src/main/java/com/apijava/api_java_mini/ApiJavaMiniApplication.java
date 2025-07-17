aeac40a78e96358db3b350eca8742bf98c6297e9
Valdison Metelis <valdison@outlook.com>
2025-07-17 11:44:12 -0300

    init

---
+++ src/main/java/com/apijava/api_java_mini/ApiJavaMiniApplication.java
@@ -0,0 +1,13 @@
+package com.apijava.api_java_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apijava/api_java_mini/ApiJavaMiniApplicationTests.java
@@ -0,0 +1,15 @@
+package com.apijava.api_java_mini;
+import org.springframework.test.context.ActiveProfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ApiJavaMiniApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
